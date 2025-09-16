7bd2026d69f19d68021004fa3c5c40fd970720e8
Aboje Edwin <abojeedwin@gmail.com>
2025-09-16 02:21:14 +0100

    Artist login service

--- data/models/Fan.js
+++ data/models/Fan.js
@@ -3,18 +3,16 @@ import Role from "../../enum/Role";
 
 const fanSchema = new mongoose.Schema(
     {
+
         firstName: { type: String },
         lastName: { type: String },
         email: { type: String, required: true, unique: true },
         locale: { type: String },
         picture: { type: String },
-        provider: {
-            type: String,
-            enum: ["google", "facebook", "zklogin"],
-            required: true,
-        },
+        provider: {type: String, enum: ["google", "facebook", "zklogin"], required: true},
+        suiAddress: {type: String, required: true, unique: true},
+        suiPrivateKey: {type: String, required: true},
         providerId: { type: String, required: true },
-        zkAddress: { type: String },
         role: {type:String, enum: Object.values(Role), default: Role.FAN}
     },
     { timestamps: true }
