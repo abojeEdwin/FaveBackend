d93960d27fe886785a3f3db3d50112c2002097a2
Aboje Edwin <abojeedwin@gmail.com>
2025-09-16 16:03:39 +0100

    Project restructure

--- controller/ArtistController.js
+++ controller/ArtistController.js
@@ -7,23 +7,24 @@ import {generateSessionToken} from "../utils/helper";
 
 
 
+
 export const login = async (req, res) => {
     try {
-        const { idToken } = req.body;
+        const {idToken} = req.body;
         if (!idToken) {
-            return res.status(400).json({ error: "Missing idToken" });}
+            return res.status(400).json({error: "Missing idToken"});}
         const decodedToken = await verifyJWT.verifyJWT(idToken);
-        const { sub: authProviderId, email, name, picture } = decodedToken;
+        const {sub: authProviderId, email, name, picture} = decodedToken;
 
-        let foundArtist = await Artist.findOne({ authProviderId });
+        let foundArtist = await Artist.findOne({authProviderId});
         if (!foundArtist) {
             const keypair = new Ed25519Keypair();
             const suiAddress = keypair.getPublicKey().toSuiAddress();
-            const artist = new Artist({
+            let artist = new Artist({
                 suiAddress: suiAddress,
                 authProviderId: authProviderId,
                 authProvider: "google",
-                profile: { name, email, picture },
+                profile: {name, email, picture},
                 role: Role.ARTIST,
                 distributorLink: "",
                 nin: "",
@@ -31,12 +32,11 @@ export const login = async (req, res) => {
                 verificationStatus: Status.PENDING,
                 suiPrivateKey: keypair.getSecretKey().toString("base64"),
             });
-            await Artist.save();
-        } else {
+            await artist.save();
+        }else{
             foundArtist.lastLogin = new Date();
-            await Artist.save();
+            await artist.save();
         }
-
         const sessionToken = generateSessionToken(foundArtist._id);
         res.json({
             success: true,
@@ -47,8 +47,9 @@ export const login = async (req, res) => {
                 profile: foundArtist.profile,
             },
         });
-    } catch (error) {
+    }catch(error){
         console.error("zkLogin authentication error:", error);
-        res.status(401).json({ success: false, error: error.message });
+        res.status(401).json({success: false, error: error.message});
     }
+
 }
