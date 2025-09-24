da1965a997561476d13aadd91842c18cd737ef63
Aboje Edwin <abojeedwin@gmail.com>
2025-09-24 05:20:13 +0100

    Fixing mixed up files

--- route/FanRoute.js
+++ route/FanRoute.js
@@ -1,11 +1,8 @@
 import express from "express";
-import {login, buySong, viewListedSongs, viewArtistProfile} from "../controller/FanController.js";
+import {login, buySong} from "../controller/FanController.js";
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/buySong", buySong);
-router.get("/listedSongs", viewListedSongs);
-router.get("/artistProfile/:artistId", viewArtistProfile);
-
 export default router;
